2e398b999158717469120e17c4f973ef9db89966
ABDULLAH SHEIKH <abdullahmajid5678@gmail.com>
2024-12-18 18:58:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+                                // JAVASCRIPT 
+                            //  ASSIGNMENT 1  chapter 1
+
+// QUESTION 1
+alert("hello dear!")
+
+// QUESTION 2
+alert("Error! please try again")
+
+// QUESTION 3
+alert("welcome to js land...\n happy coding!")
+
+// QUESTION 4
+alert("welcome to js land")
+alert("happy coding!")
+
+// QUESTION 5
+console.log("Hello... I can run JS through my web browser's console")
+
+
+                                  //  ASSIGNMENT 1 chapter 2
+
+// QUESTION 1
+
+     var username;
+     
+// QUESTION 2     
+
+     var myname="Abdullah";
+ 
+// QUESTION 3
+
+    var message="hello world!";
+    alert(message);
+
+ // QUESTION 4
+    
+const studentName = "Abdullah!";
+ const studentAge = 17;
+ const studentCourse = "web and mobile app development";
+ alert(studentName);
+ alert(studentAge);
+ alert(studentCourse);
+
+ // QUESTION 5
+
+    const display = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+    alert(display);
+
+ // QUESTION 6
+ 
+     const email = "abdullahmajid5678@gmail.com";
+     alert(email);
+ 
+ // QUESTION 7
+
+        const book = "A smarter way to learn javascript";
+         alert(book);
+                     
+ // QUESTION 8
+                                
+         document.write("Yah! I can write HTML content through JavaScript")                       
